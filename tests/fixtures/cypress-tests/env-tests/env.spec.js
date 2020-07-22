949078ac9d0425d1b26e6750fa4b42ab3687d836
Dan <dpgraham.85@gmail.com>
2020-07-22 13:40:41 -0700

    Bump package version

--- tests/fixtures/cypress-tests/env-tests/env.spec.js
+++ tests/fixtures/cypress-tests/env-tests/env.spec.js
@@ -5,8 +5,6 @@ context('Actions', () => {
       cy.visit('https://example.cypress.io/commands/actions')
     })
   
-    // https://on.cypress.io/interacting-with-elements
-  
     it('should use .env.json', () => {
       expect(Cypress.env('foo')).to.equal('BAR');
     })
