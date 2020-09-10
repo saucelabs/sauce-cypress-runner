58fc7cfc970a8dfe5ae99bf69cfa0b954b6d0acf
vrunoa <vruno.alassia@gmail.com>
2020-09-10 08:19:04 -0700

    fix identation

--- src/sauce-reporter.js
+++ src/sauce-reporter.js
@@ -96,10 +96,10 @@ exports.sauceReporter = async (buildName, browserName, spec) => {
       assets
     ).then(
       (resp) => {
-        if (resp.errors) {
-          for(let err of resp.errors) { console.warn(err)) }
-        }
-      },
+				if (resp.errors) {
+					for(let err of resp.errors) { console.warn(err)) }
+				}
+			},
       (e) => console.log('upload failed:', e.stack)
     ),
     api.updateJob(process.env.SAUCE_USERNAME, sessionId, {
