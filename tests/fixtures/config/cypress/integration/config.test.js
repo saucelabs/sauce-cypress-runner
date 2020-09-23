4b3e0f1789e7f8af705af6784d92f3895d91971c
Dan <dpgraham.85@gmail.com>
2020-09-22 17:13:05 -0700

    Add saucectl tests

--- tests/fixtures/config/cypress/integration/config.test.js
+++ tests/fixtures/config/cypress/integration/config.test.js
@@ -1,15 +1,14 @@
 /// <reference types="cypress" />
 
-context('Actions', () => {
-    beforeEach(() => {
-      cy.visit('https://example.cypress.io/commands/actions')
-    })
-  
-    // https://on.cypress.io/interacting-with-elements
-  
-    it('should use cypress.json', () => {
-      console.log('!!!!!', Cypress.env('foo'))
-      expect(Cypress.env('foo')).to.equal('bar');
-    })
-  })
-  
+context('Actions', function () {
+  beforeEach(function () {
+    cy.visit('https://example.cypress.io/commands/actions');
+  });
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it('should use cypress.json', function () {
+    console.log('!!!!!', Cypress.env('foo'));
+    expect(Cypress.env('foo')).to.equal('bar');
+  });
+});
