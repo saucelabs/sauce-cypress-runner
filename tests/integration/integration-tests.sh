36a18c1ad42976669ad1178e85f7f0d3da1030c4
Dan <dpgraham.85@gmail.com>
2020-09-22 17:13:05 -0700

    Add saucectl tests

--- tests/integration/integration-tests.sh
+++
@@ -1,6 +0,0 @@
-export SAUCE_USERNAME=''
-export SAUCE_ACCESS_KEY=''
-export CONFIG_FILE=./tests/config-tests/config.yaml && node .
-export CONFIG_FILE=./tests/env-tests/config.yaml && node .
-export CONFIG_FILE=./tests/typescript-tests/config.yaml && node .
-export CONFIG_FILE=./tests/typescript-no-config-tests/config.yaml && node .
