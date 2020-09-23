f7a3732315be2bbecdbdddf26f30caedf3905339
Dan <dpgraham.85@gmail.com>
2020-09-22 21:20:00 -0700

    Again

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,14 +1,15 @@
-rm -rf bundle/
-mkdir bundle/
-export CYPRESS_CACHE_FOLDER=bundle/Cache/
+rm -rf ./bundle/
+mkdir ./bundle/
+export CYPRESS_CACHE_FOLDER=$PWD/bundle/Cache/
+echo $CYPRESS_CACHE_FOLDER
 npm ci --production
-cp -r node_modules/ bundle/node_modules/
-cp -r src/ bundle/src/
-cp -r package.json bundle/
+cp -r ./node_modules/ ./bundle/node_modules/
+cp -r ./src/ ./bundle/src/
 cp -r bin/ bundle/bin/
-cp config.yaml bundle/
-cp cypress.json bundle/
-cp $(which node) bundle/
+cp package.json bundle/package.json
+cp config.yaml bundle/config.yaml
+cp cypress.json bundle/cypress.json
+cp $(which node) bundle/node
 
 pushd bundle/
 ./node ./node_modules/cypress/bin/cypress verify
