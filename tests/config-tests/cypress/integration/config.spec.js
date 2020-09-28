36db8db8cb9d1e842f750799fc10bece6dc2b152
Dan <dpgraham.85@gmail.com>
2020-09-28 11:11:55 -0700

    Add fixes

--- tests/config-tests/cypress/integration/config.spec.js
+++ tests/config-tests/cypress/integration/config.spec.js
@@ -8,7 +8,6 @@ context('Actions', function () {
   // https://on.cypress.io/interacting-with-elements
 
   it('should use cypress.json', function () {
-    console.log('!!!!!', Cypress.env('foo'));
     expect(Cypress.env('foo')).to.equal('bar');
   });
 });
