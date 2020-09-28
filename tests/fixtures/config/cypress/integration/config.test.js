ae02bf090a828477793659373ddae50c8552382f
Dan <dpgraham.85@gmail.com>
2020-09-28 10:18:24 -0700

    rebase

--- tests/fixtures/config/cypress/integration/config.test.js
+++ tests/fixtures/config/cypress/integration/config.test.js
@@ -1,13 +1,15 @@
 /// <reference types="cypress" />
 
-context('Actions', function () {
-  beforeEach(function () {
-    cy.visit('https://example.cypress.io/commands/actions');
-  });
-
-  // https://on.cypress.io/interacting-with-elements
-
-  it('should use .env.json', function () {
-    expect(Cypress.env('foo')).to.equal('bar');
-  });
-});
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('https://example.cypress.io/commands/actions')
+    })
+  
+    // https://on.cypress.io/interacting-with-elements
+  
+    it('should use cypress.json', () => {
+      console.log('!!!!!', Cypress.env('foo'))
+      expect(Cypress.env('foo')).to.equal('bar');
+    })
+  })
+  
