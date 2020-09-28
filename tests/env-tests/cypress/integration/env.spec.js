9d9d8b6f03c6b89e82fc108439427fdaf01338d0
Dan <dpgraham.85@gmail.com>
2020-09-28 10:18:24 -0700

    rebase

--- tests/env-tests/cypress/integration/env.spec.js
+++ tests/env-tests/cypress/integration/env.spec.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
-context('Actions', function () {
-  beforeEach(function () {
-    cy.visit('https://example.cypress.io/commands/actions');
-  });
-
-  it('should use .env.json', function () {
-    expect(Cypress.env('foo')).to.equal('BAR');
-  });
-});
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('https://example.cypress.io/commands/actions')
+    })
+  
+    it('should use .env.json', () => {
+      expect(Cypress.env('foo')).to.equal('BAR');
+    })
+  })
+  
