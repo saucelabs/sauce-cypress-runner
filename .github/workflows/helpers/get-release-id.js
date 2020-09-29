29aecac84f2cdf379f4e7989b40cfa2ce93dbaae
Dan <dpgraham.85@gmail.com>
2020-09-29 00:35:14 -0700

    Again

--- .github/workflows/helpers/get-release-id.js
+++ .github/workflows/helpers/get-release-id.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 
 async function getReleaseId (tag) {
     const url = `https://api.github.com/repos/saucelabs/sauce-cypress-runner/releases/tags/${tag}`;
-    console.log(`Release URL ${url}`);
     const res = await axios.get(url);
     console.log(res.data.id);
 };
@@ -13,7 +12,6 @@ if (require.main === module) {
   if (type != 'tag') {
     value = 'v0.1.9'; // <-- for testing purposes
   }
-  console.log(`Getting release id for tag '${value}'`);
   getReleaseId(value)
     .then(() => process.exit(0))
     .catch(() => process.exit(1));
