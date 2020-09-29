923a31cd36f5fb372d96ffba08fbeb0423300920
Dan <dpgraham.85@gmail.com>
2020-09-28 17:54:46 -0700

    Try again

--- .github/workflows/helpers/upload-github-release.js
+++ .github/workflows/helpers/upload-github-release.js
@@ -6,18 +6,15 @@ exec = promisify(exec);
 
 async function uploadGithubRelease (tag, file) {
     const githubRelease = path.join(process.env.GOPATH, 'bin', 'github-release');
-    console.log('Uploading to GitHub releases');
+    console.log(`Uploading to GitHub releases: tag=${tag} file=${file}`);
     await exec(`${githubRelease} upload --user saucelabs --repo sauce-cypress-runner ` +
       `--tag ${tag} --name ${file} --file ${file}`);
     console.log('Done releasing to GitHub');
 };
 
 if (require.main === module) {
-  let [,, tag, file] = process.argv;
-  if (!file) {
-      file = tag;
-      tag = 'v0.1.9'; // If no tag provided, hardcode v0.1.9 (for testing purposes only)
-  }
+  let tag = process.env.GH_TAG || 'v0.1.9';
+  let file = process.env.GH_FILE;
   uploadGithubRelease(tag, file)
     .then(() => process.exit(0))
     .catch(() => process.exit(1));
