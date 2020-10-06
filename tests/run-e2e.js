f3d6ed9270a296fce64176068963e0de782a0272
Dan <dpgraham.85@gmail.com>
2020-10-06 14:03:35 -0700

    Again

--- tests/run-e2e.js
+++ tests/run-e2e.js
@@ -1,13 +1,17 @@
 const { execSync } = require('child_process');
 const path = require('path');
-const glob = require('glob-fs')();
+const fs = require('fs');
 
-const dirs = glob.readdirSync(path.join('tests', 'fixtures', 'projects', '*-tests/'));
+const projectsDir = './tests/fixtures/projects/';
+const dirs = fs.readdirSync(projectsDir);
 
 for (const dir of dirs) {
-  let SAUCE_CTL_BINARY = process.env.SAUCE_CTL_BINARY;
-  if (!path.isAbsolute(SAUCE_CTL_BINARY)) {
-    SAUCE_CTL_BINARY = path.join(process.cwd(), process.env.SAUCE_CTL_BINARY);
+  if (dir.endsWith('-tests')) {
+    let projectDir = path.join(process.cwd(), projectsDir, dir);
+    let SAUCE_CTL_BINARY = process.env.SAUCE_CTL_BINARY;
+    if (!path.isAbsolute(SAUCE_CTL_BINARY)) {
+      SAUCE_CTL_BINARY = path.join(process.cwd(), process.env.SAUCE_CTL_BINARY);
+    }
+    execSync(`${SAUCE_CTL_BINARY} run`, {cwd: projectDir, stdio: 'inherit'});
   }
-  execSync(`${SAUCE_CTL_BINARY} run`, {cwd: path.join(process.cwd(), dir), stdio: 'inherit'});
 }
