2537a82a14def16ac63f81f024b57ccce4ece602
Dan <dpgraham.85@gmail.com>
2020-10-06 11:43:33 -0700

    again

--- tests/run-e2e.js
+++ tests/run-e2e.js
@@ -5,6 +5,9 @@ const glob = require('glob-fs')();
 const dirs = glob.readdirSync(path.join('tests', 'fixtures', 'projects', '*-tests/'));
 
 for (const dir of dirs) {
-  const SAUCE_CTL_BINARY = process.env.SAUCE_CTL_BINARY;
+  let SAUCE_CTL_BINARY = process.env.SAUCE_CTL_BINARY;
+  if (!path.isAbsolute(SAUCE_CTL_BINARY)) {
+    SAUCE_CTL_BINARY = path.join(process.cwd(), process.env.SAUCE_CTL_BINARY);
+  }
   execSync(`${SAUCE_CTL_BINARY} run`, {cwd: path.join(process.cwd(), dir), stdio: 'inherit'});
 }
