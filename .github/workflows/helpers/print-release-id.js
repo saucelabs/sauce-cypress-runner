100865925856389a6275e5d57306e16c7f35e89f
Felix P <felix.pescarmona@saucelabs.com>
2020-10-19 12:22:03 -0700

    Remove dependencie for external libs + fix format for archive

--- .github/workflows/helpers/print-release-id.js
+++ .github/workflows/helpers/print-release-id.js
@@ -1,22 +1,37 @@
-const axios = require('axios');
+
+const { get } = require('https');
 
 /**
  * Get the release associated with a tag and print the release ID to console
  * @param {string} tag 
  */
-async function printReleaseId (tag) {
-    const url = `https://api.github.com/repos/saucelabs/sauce-cypress-runner/releases/tags/${tag}`;
-    const res = await axios.get(url);
-    console.log(res.data.id);
-};
+const printReleaseId = (repo, tag) => new Promise(async (resolve) => {
+  get(`https://api.github.com/repos/${repo}/releases/tags/${tag}`, {
+    headers: {
+      'Accept': 'application/vnd.github.v3+json',
+      'User-Agent': 'ReleaseId Fetcher',
+    }
+  }, (res) => {
+    let body = '';
+    res.on('data', (chunk) => {
+      body += chunk;
+    });
+    res.on('end', () => {
+      body = JSON.parse(body);
+      console.log(body.id);
+      resolve();
+    });
+  });
+});
 
 if (require.main === module) {
+  let repo = process.env.GH_REPO;
   let ref = process.env.GH_REF;
-  let [,type,value] = ref.split('/');
-  if (type != 'tag') {
-    value = 'v0.1.9'; // <-- for testing purposes
+  let [,type,tag] = ref.split('/');
+  if (!['tag', 'tags'].includes(type)) {
+    tag = 'v0.1.9'; // <-- for testing purposes
   }
-  printReleaseId(value)
+  printReleaseId(repo, tag)
     .then(() => process.exit(0))
     .catch(() => process.exit(1));
 }
