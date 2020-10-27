9d975b12892f00e921d24e3a5abbcbc1d16a0e51
Dan <dpgraham.85@gmail.com>
2020-10-27 13:56:54 -0700

    Again

--- src/cypress-runner.js
+++ src/cypress-runner.js
@@ -57,61 +57,69 @@ const report = async (results) => {
 };
 
 const cypressRunner = async function () {
-  let {rootDir, reportsDir, targetDir} = await getRunnerConfig();
+  try {
+    // Get the configuration info from config.yaml
+    const configYamlDefault = process.env.SAUCE_RUNNER_LOCAL ? 'config-local.yaml' : 'config.yaml';
+    const configYamlPath = process.env.CONFIG_FILE || configYamlDefault;
+    const config = yaml.safeLoad(await readFile(configYamlPath, 'utf8'));
 
-  const runCfgPath = path.join(rootDir, 'run.yaml');
-  const runCfg = await loadRunConfig(runCfgPath);
+    // If relative paths were provided in YAML, convert them to absolute
+    const reportsDir = getAbsolutePath(config.reportsDir);
 
-  if (!runCfg.projectPath) {
-    runCfg.projectPath = targetDir;
-  }
+    const runCfgPath = path.join(config.rootDir, 'run.yaml');
+    const runCfg = await loadRunConfig(runCfgPath);
 
-  // If a typescript config is found in the project path, then compile with it
-  const tsconfigPath = path.join(runCfg.projectPath, 'tsconfig.json');
+    // If a typescript config is found in the project path, then compile with it
+    const tsconfigPath = path.join(runCfg.projectPath, 'tsconfig.json');
 
-  if (await fileExists(tsconfigPath)) {
-    console.log(`Compiling Typescript files from tsconfig '${tsconfigPath}'`);
-    await exec(`npx tsc -p "${tsconfigPath}"`);
-  }
-
-  // Get the cypress.json config file (https://docs.cypress.io/guides/references/configuration.html#Options)
-  let configFile = 'cypress.json';
-  let cypressJsonPath = path.join(runCfg.projectPath, 'cypress.json');
-  if (await fileExists(cypressJsonPath)) {
-    configFile = path.relative(process.cwd(), cypressJsonPath);
-  }
-
-  // Get the cypress env variables from 'cypress.env.json' (if present)
-  let env = {};
-  const cypressEnvPath = path.join(runCfg.projectPath, 'cypress.env.json');
-  if (await fileExists(cypressEnvPath)) {
-    try {
-      env = JSON.parse(await readFile(cypressEnvPath));
-    } catch (e) {
-      console.error(`Could not parse contents of '${cypressEnvPath}'. Will use empty object for environment variables.`);
+    if (await fileExists(tsconfigPath)) {
+      console.log(`Compiling Typescript files from tsconfig '${tsconfigPath}'`);
+      await exec(`npx tsc -p "${tsconfigPath}"`);
     }
-  }
 
-  const results = await cypress.run({
-    browser: browserName,
-    configFile,
-    config: {
-      env,
-      video: true,
-      videosFolder: reportsDir,
-      videoCompression: false,
-      videoUploadOnPasses: false,
-      screenshotsFolder: reportsDir,
-      integrationFolder: runCfg.projectPath,
-      testFiles: runCfg.match,
-      reporter: path.join('src', 'custom-reporter.js'),
-      reporterOptions: {
-        mochaFile: path.join(reportsDir, '[suite].xml'),
-        specFolder: runCfg.projectPath,
-      },
+    // Get the cypress.json config file (https://docs.cypress.io/guides/references/configuration.html#Options)
+    let configFile = 'cypress.json';
+    let cypressJsonPath = path.join(runCfg.projectPath, 'cypress.json');
+    if (await fileExists(cypressJsonPath)) {
+      configFile = path.relative(process.cwd(), cypressJsonPath);
     }
-  });
-  return await report(results);
+
+    // Get the cypress env variables from 'cypress.env.json' (if present)
+    let env = {};
+    const cypressEnvPath = path.join(runCfg.projectPath, 'cypress.env.json');
+    if (await fileExists(cypressEnvPath)) {
+      try {
+        env = JSON.parse(await readFile(cypressEnvPath));
+      } catch (e) {
+        console.error(`Could not parse contents of '${cypressEnvPath}'. Will use empty object for environment variables.`);
+      }
+    }
+
+    const results = await cypress.run({
+      browser: browserName,
+      configFile,
+      config: {
+        env,
+        video: shouldRecordVideo(),
+        videosFolder: reportsDir,
+        videoCompression: false,
+        videoUploadOnPasses: false,
+        screenshotsFolder: reportsDir,
+        integrationFolder: runCfg.projectPath,
+        testFiles: runCfg.match,
+        reporter: 'src/custom-reporter.js',
+        reporterOptions: {
+          mochaFile: `${reportsDir}/[suite].xml`,
+          specFolder: runCfg.projectPath,
+        },
+      }
+    });
+    const status = await report(results);
+    process.exit(status);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
 
 // For dev and test purposes, this allows us to run our Cypress Runner from command line
