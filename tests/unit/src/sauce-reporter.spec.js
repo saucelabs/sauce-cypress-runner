e6dfcdb8d677f868697f993bde211bb0a5194c35
Félix P <felix.pescarmona@saucelabs.com>
2020-10-27 10:14:59 -0700

    feat: Export console output to SauceLabs (#45)
    * Record console & upload to saucelabs

--- tests/unit/src/sauce-reporter.spec.js
+++ tests/unit/src/sauce-reporter.spec.js
@@ -30,6 +30,7 @@ describe('SauceReporter', function () {
       expect(res).toEqual([
         'tmp/folder/video.mp4',
         'tmp/folder/log.json',
+        'tmp/folder/console.log',
         'tmp/folder/junit.xml'
       ]);
     });
