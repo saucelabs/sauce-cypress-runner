c3788c79b02c62cb889ade279eafc1b9a53bd9f9
Tian Feng <tian.feng@saucelabs.com>
2020-11-04 14:44:20 -0800

    test against ci env

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -3,9 +3,6 @@
 export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
-export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests
-npm install saucectl && \
-  export PATH=$PATH:$PWD/node_modules/.bin && \
-  cd $SAUCE_ROOT_DIR && \
-  saucectl run
-
+export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && \
+  echo '{"fixturesFolder":"tests/kitchen-sink-tests/cypress/fixtures"}' > cypress.json && \
+  node .
