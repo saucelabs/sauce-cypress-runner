0e404439972fd424370dfef3a2b621f5108d6e62
Tian Feng <tian.feng@saucelabs.com>
2020-11-04 14:32:34 -0800

    fix pipeline

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -3,4 +3,9 @@
 export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
-export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && node .
+export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests
+npm install saucectl && \
+  export PATH=$PATH:$PWD/node_modules/.bin && \
+  cd $SAUCE_ROOT_DIR && \
+  saucectl run
+
