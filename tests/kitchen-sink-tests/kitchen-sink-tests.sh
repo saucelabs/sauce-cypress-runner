06c2078694364a40eee5ca0898a58093e958b234
Tian Feng <tian.feng@saucelabs.com>
2020-11-05 13:49:50 -0800

    echo env var

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -3,8 +3,9 @@
 export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
+echo "CI is " && echo $CI
 export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && \
   npm install saucectl && \
   export PATH=$PATH:$PWD/node_modules/.bin/ && \
-  cp ./config.yaml /home/seluser/ && \
+  #cp ./config.yaml /home/seluser/ && \
   saucectl run
