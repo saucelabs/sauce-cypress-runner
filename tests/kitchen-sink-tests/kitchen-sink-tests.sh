fce9463e16357be6ead9e06b45e89999fdc0ab28
vrunoa <vruno.alassia@gmail.com>
2020-11-04 18:03:11 -0800

    change cypress.json path

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -4,5 +4,5 @@ export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
 export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && \
-  echo '{"fixturesFolder":"tests/kitchen-sink-tests/cypress/fixtures"}' > cypress.json && \
+  echo '{"fixturesFolder":"tests/kitchen-sink-tests/cypress/fixtures"}' > tests/kitchen-sink-tests/cypress.json && \
   node .
