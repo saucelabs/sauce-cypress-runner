294ba4807c2ef9e349669f9d286420ccd18d65e1
Tian Feng <tian.feng@saucelabs.com>
2020-11-05 16:16:34 -0800

    fix user permission

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -4,6 +4,6 @@ export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
 export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && \
+  sudo chown -R $USER:$(id -gn $USER) $SAUCE_ROOT_DIR && \
   cd $SAUCE_ROOT_DIR && \
-  sudo touch console.log && sudo chown $USER:$(id -gn $USER) console.log && \
   saucectl run
