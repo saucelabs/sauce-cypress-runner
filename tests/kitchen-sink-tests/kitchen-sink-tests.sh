b1e56462e60f798023d6b6160b34699f25cc7ba4
Tian Feng <tian.feng@saucelabs.com>
2020-11-05 16:11:37 -0800

    touch console.log and update permission

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -5,4 +5,5 @@ export SAUCE_ACCESS_KEY=''
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
 export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && \
   cd $SAUCE_ROOT_DIR && \
+  sudo touch console.log && sudo chown $USER:$(id -gn $USER) console.log && \
   saucectl run
