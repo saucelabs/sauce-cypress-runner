57540e7b78bd344a4fa97f5a655db7021d549f36
Tian Feng <tian.feng@saucelabs.com>
2020-11-05 16:21:11 -0800

    chown for report dir

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -3,7 +3,8 @@
 export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export SAUCE_REPORTS_DIR=$PWD/'__assets__'
-export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests && \
-  sudo chown -R $USER:$(id -gn $USER) $SAUCE_ROOT_DIR && \
-  cd $SAUCE_ROOT_DIR && \
+export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests 
+sudo chown -R $USER:$(id -gn $USER) $SAUCE_ROOT_DIR 
+sudo chown -R $USER:$(id -gn $USER) $SAUCE_REPORTS_DIR
+cd $SAUCE_ROOT_DIR && \
   saucectl run
