a8bbd37ba4be320be99e28b8604b312eb8c66dbd
Tian Feng <tian.feng@saucelabs.com>
2020-11-06 13:00:07 -0800

    update bash script

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -7,5 +7,4 @@ export SAUCE_REPORTS_DIR=$SAUCE_ROOT_DIR/'__assets__'
 sudo chown -R $USER:$(id -gn $USER) $SAUCE_ROOT_DIR 
 sudo mkdir -p $SAUCE_REPORTS_DIR && sudo chown -R $USER:$(id -gn $USER) $SAUCE_REPORTS_DIR
 cd $SAUCE_ROOT_DIR && \
-  echo '{"fixturesFolder": "./cypress/fixtures"}' > cypress.json && \
   saucectl run
