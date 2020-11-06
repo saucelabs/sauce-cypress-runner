c336951faaee72d771718c1f9210d68a30aa0fd8
Tian Feng <tian.feng@saucelabs.com>
2020-11-05 18:01:02 -0800

    create __asset__ dir

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -5,6 +5,6 @@ export SAUCE_ACCESS_KEY=''
 export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests 
 export SAUCE_REPORTS_DIR=$SAUCE_ROOT_DIR/'__assets__'
 sudo chown -R $USER:$(id -gn $USER) $SAUCE_ROOT_DIR 
-sudo chown -R $USER:$(id -gn $USER) $SAUCE_REPORTS_DIR
+sudo mkdir -p $SAUCE_REPORTS_DIR && sudo chown -R $USER:$(id -gn $USER) $SAUCE_REPORTS_DIR
 cd $SAUCE_ROOT_DIR && \
   saucectl run
