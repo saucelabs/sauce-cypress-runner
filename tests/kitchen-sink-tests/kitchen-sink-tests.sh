686181f2d80e3a7d22c1a43e55bcd539efc83fa7
Tian Feng <tian.feng@saucelabs.com>
2020-11-09 11:30:45 -0800

    update github action config

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -4,7 +4,5 @@ export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
 export WORKING_DIR=$PWD/tests/kitchen-sink-tests 
 export WORKING_RPT_DIR=$WORKING_DIR/'__assets__'
-sudo chown -R $USER:$(id -gn $USER) $WORKING_DIR 
-sudo mkdir -p $WORKING_RPT_DIR && sudo chown -R $USER:$(id -gn $USER) $WORKING_RPT_DIR
 cd $WORKING_DIR && \
   saucectl run --verbose
