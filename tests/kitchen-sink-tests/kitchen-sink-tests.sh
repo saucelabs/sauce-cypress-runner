1ba8c693d640160077ea6669723eac61a30760f5
Tian Feng <tian.feng@saucelabs.com>
2020-11-09 11:03:59 -0800

    update test method

--- tests/kitchen-sink-tests/kitchen-sink-tests.sh
+++ tests/kitchen-sink-tests/kitchen-sink-tests.sh
@@ -2,10 +2,9 @@
 
 export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY='' 
-export SAUCE_ROOT_DIR=$PWD/tests/kitchen-sink-tests 
-export SAUCE_REPORTS_DIR=$SAUCE_ROOT_DIR/'__assets__'
-sudo chown -R $USER:$(id -gn $USER) $SAUCE_ROOT_DIR 
-sudo mkdir -p $SAUCE_REPORTS_DIR && sudo chown -R $USER:$(id -gn $USER) $SAUCE_REPORTS_DIR
-cd $SAUCE_ROOT_DIR && \
-  echo '{"fixturesFolder":"./cypress/fixtures"}' > cypress.json && \
+export WORKING_DIR=$PWD/tests/kitchen-sink-tests 
+export WORKING_RPT_DIR=$WORKING_DIR/'__assets__'
+sudo chown -R $USER:$(id -gn $USER) $WORKING_DIR 
+sudo mkdir -p $WORKING_RPT_DIR && sudo chown -R $USER:$(id -gn $USER) $WORKING_RPT_DIR
+cd $WORKING_DIR && \
   saucectl run --verbose
