503a070f94ff1e40b23126740e91ed15c149bae4
Alex Plischke <alex.plischke@saucelabs.com>
2020-11-18 15:35:04 -0800

    Fix integration tests

--- tests/integration/integration-tests.sh
+++ tests/integration/integration-tests.sh
@@ -1,5 +1,4 @@
 export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY=''
-export SAUCE_REPORTS_DIR=$PWD/'__assets__'
-export SAUCE_ROOT_DIR=$PWD/tests/config-tests && node .
-export SAUCE_ROOT_DIR=$PWD/tests/env-tests && node .
+pushd tests/config-tests/ && node ../../ -r sauce-runner.json -s "default" && popd
+pushd tests/env-tests/ && node ../../ -r sauce-runner.json -s "default" && popd
