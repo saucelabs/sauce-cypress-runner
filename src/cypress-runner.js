976f7c1aa0a8459c80cb720d3b39beb6cd3f2906
Félix P <felix@saucelabs.com>
2020-11-20 15:07:29 -0800

    Simplify handler

--- src/cypress-runner.js
+++ src/cypress-runner.js
@@ -44,7 +44,7 @@ const installDependencies = function (runCfg) {
     const child = child_process.spawn(path.join(path.dirname(process.argv[0]), 'npm'), ['install', '--no-save', ...packageList]);
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
-    child.on('exit', () => resolve());
+    child.on('exit', resolve);
   });
 };
 
