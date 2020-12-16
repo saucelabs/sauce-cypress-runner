f9fe050132583ac5b899a6bd2dac71de5994ca2d
Dan <dpgraham.85@gmail.com>
2020-12-16 01:43:12 -0800

    feat: make installDependencies work on VM

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const childProcess = require('child_process');
 
 function getAbsolutePath (pathToDir) {
   if (path.isAbsolute(pathToDir)) {
@@ -24,4 +25,35 @@ function loadRunConfig (cfgPath) {
   throw new Error(`Runner config (${cfgPath}) unavailable.`);
 }
 
-module.exports = { getAbsolutePath, shouldRecordVideo, loadRunConfig };
+async function installDependencies (runCfg) {
+  const npmConfig = runCfg && runCfg.npm && runCfg.npm.packages || {};
+  const packageList = Object.entries(npmConfig).map(([pkg, version]) => `${pkg}@${version}`);
+
+  if (packageList.length === 0) {
+    return;
+  }
+
+  const p = new Promise((resolve, reject) => {
+    const nodeBin = process.platform === 'win32' ? 'node.exe' : 'node';
+    const nodePath = path.join(__dirname, '..', nodeBin);
+    const npmCli = path.join(__dirname, '..', 'node_modules', 'npm', 'bin', 'npm-cli');
+    const npmArgs = ['install', '--no-save', ...packageList];
+    const procArgs = process.env.SAUCE_VM ?
+      [nodePath, npmCli, ...npmArgs] :
+      [path.join(path.dirname(process.argv[0]), 'npm'), ...npmArgs];
+    console.log(`Running npm install on ${npmArgs.join(', ')}`);
+    const child = childProcess.spawn(procArgs[0], procArgs.slice(1));
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.on('exit', (exitCode) => {
+      if (exitCode === 0) {
+        resolve();
+      } else {
+        reject(`Could not install NPM dependencies`);
+      }
+    });
+  });
+  return await p;
+}
+
+module.exports = { getAbsolutePath, shouldRecordVideo, loadRunConfig, installDependencies };
