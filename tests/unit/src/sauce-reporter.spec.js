4ee2f2074211e4feff6f0d29119075f16509f0e8
Tian Feng <tian.feng@saucelabs.com>
2021-01-06 16:14:31 -0800

    Revert "add function to call creating job API from test-composer (#69)"
    This reverts commit 0e0ebb78335c084c2bc5f5d1404336b80792eea3.

--- tests/unit/src/sauce-reporter.spec.js
+++ tests/unit/src/sauce-reporter.spec.js
@@ -14,9 +14,6 @@ describe('SauceReporter', function () {
       }
     }
   };
-  const start = new Date().toISOString();
-  const end = new Date().toISOString();
-
   describe('.prepareAssets', function () {
     beforeEach(function () {
       fs.existsSync.mockClear();
@@ -38,19 +35,13 @@ describe('SauceReporter', function () {
     });
   });
   describe('.sauceReporter', function () {
-    let prepareAssetsSpy, uploadJobAssetsSpy, createJobSpy, createResultSpy, createJobWorkaroundSpy, backupEnv = process.env;
+    let prepareAssetsSpy, uploadJobAssetsSpy, createJobSpy, backupEnv = process.env;
     beforeEach(function () {
-      createJobWorkaroundSpy = jest.spyOn(SauceReporter, 'createJobWorkaround');
-      // eslint-disable-next-line require-await
-      createJobWorkaroundSpy.mockImplementation(async () => 'fake-session-id');
       webdriverio.remote.mockImplementation(function () {});
       prepareAssetsSpy = jest.spyOn(SauceReporter, 'prepareAssets');
-      createJobWorkaroundSpy = jest.spyOn(SauceReporter, 'createJobWorkaround');
-      createJobWorkaroundSpy.mockImplementation(async () => await 'fake-session-id');
       // eslint-disable-next-line require-await
       uploadJobAssetsSpy = jest.fn().mockImplementation(async () => ({errors: ['some fake error']}));
       createJobSpy = jest.fn().mockImplementation(async () => (await {sessionId: '123'}));
-      createResultSpy = jest.fn().mockImplementation(async () => (await {sessionId: '123'}));
       SauceLabs.default.mockImplementation(function () {
         // eslint-disable-next-line require-await
         this.listJobs = async () => ({
@@ -59,34 +50,29 @@ describe('SauceReporter', function () {
         this.uploadJobAssets = uploadJobAssetsSpy;
         this.updateJob = async () => { };
         this.createResultJob = createJobSpy;
-        this.createJob = createResultSpy;
       });
       process.env.SAUCE_USERNAME = 'fake-user';
     });
     afterEach(function () {
       process.env = backupEnv;
     });
-
     it('should call uploadJobAssets on SauceLabs api', async function () {
-      // eslint-disable-next-line require-await
-      prepareAssetsSpy.mockImplementation(async () => ['asset/one', 'asset/two']);
-      await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0, start, end);
+      prepareAssetsSpy.mockReturnValue(['asset/one', 'asset/two']);
+      await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0);
       expect(uploadJobAssetsSpy.mock.calls).toEqual([
-        ['fake-session-id', {'files': ['asset/one', 'asset/two']}]
+        ['a', {'files': ['asset/one', 'asset/two']}]
       ]);
     });
     it('should output err when upload failed', async function () {
       let consoleErrorSpy = jest.spyOn(global.console, 'error');
       prepareAssetsSpy.mockReturnValue(['asset/one', 'asset/two']);
-      //createResultSpy.mockReturnValue({sessionId: 'a'});
-      expect(await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0, start, end)).toBeUndefined();
+      expect(await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0)).toBeUndefined();
       expect(uploadJobAssetsSpy.mock.calls).toEqual([
-        ['fake-session-id', {'files': ['asset/one', 'asset/two']}]
+        ['a', {'files': ['asset/one', 'asset/two']}]
       ]);
       expect(consoleErrorSpy.mock.calls).toMatchSnapshot();
     });
     it('should not push assets when no sessionId from SauceLabs API', async function () {
-      //createResultSpy.mockReturnValue({});
       SauceLabs.default.mockImplementation(function () {
         // eslint-disable-next-line require-await
         this.listJobs = async () => ({
@@ -94,22 +80,21 @@ describe('SauceReporter', function () {
         });
         this.uploadJobAssets = uploadJobAssetsSpy;
         this.updateJob = async () => {};
-        this.createJob = createResultSpy;
       });
 
       prepareAssetsSpy.mockReturnValue(['asset/one', 'asset/two']);
-      expect(await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0, start, end)).toBeUndefined();
+      expect(await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0)).toBeDefined();
     });
     it ('should create job via global data store', async function () {
       process.env.ENABLE_DATA_STORE = 'true';
       prepareAssetsSpy.mockReturnValue(['asset/one', 'asset/two']);
-      await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0, start, end);
+      await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0);
       expect(createJobSpy.mock.calls).toMatchSnapshot();
     });
     it ('should fail when global data store throws error', async function () {
       process.env.ENABLE_DATA_STORE = 'true';
       prepareAssetsSpy.mockReturnValue(['asset/one', 'asset/two']);
-      await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0, start, end);
+      await SauceReporter.sauceReporter(fakeRunConfig, 'build', 'browser', ['asset/one', 'asset/two'], 0);
       expect(createJobSpy).toBeCalled();
     });
   });
