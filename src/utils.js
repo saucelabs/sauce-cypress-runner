ff5bd51fc2d143b7144938a927cda05408ca77b2
dpgraham <dpgraham.85@gmail.com>
2021-02-04 18:15:13 -0800

    again

--- src/utils.js
+++ src/utils.js
@@ -47,7 +47,7 @@ async function installDependencies (runCfg) {
     const npmCli = path.join(__dirname, '..', 'node_modules', 'npm', 'bin', 'npm-cli');
 
     // Log out NPM output to a .log file
-    const npmOutput = path.join('npm.log');
+    const npmOutput = 'npm.log';
     fs.writeFileSync(npmOutput);
     const npmOutFileStream = fs.createWriteStream(npmOutput);
     const npmArgs = ['install', '--no-save', ...packageList, '--json'];
