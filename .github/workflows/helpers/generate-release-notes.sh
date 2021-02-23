d12009daa73a343c0055a0c09ff5384fcc512073
Félix P <felix@saucelabs.com>
2021-02-23 10:10:17 -0800

    Support pre-releases + multi-branch + ensure workflow run exists

--- .github/workflows/helpers/generate-release-notes.sh
+++ .github/workflows/helpers/generate-release-notes.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
 
-## Add Changelog
-echo "## Changelog"
-git --no-pager log --no-notes --no-decorate --oneline  v${1}...master
+## Gather Changelog
+CHANGELOG=$(git --no-pager log --no-notes --no-decorate --oneline  v${1}...master)
 
-## Add Framework version
+## Gather Framework version
 CYPRESS_VER=$(< package-lock.json jq -r '.dependencies["cypress"].version')
 NODEJS_VER=$(grep NODE_VERSION= Dockerfile | cut -d '=' -f 2)
 
-echo ""
-echo "## Frameworks"
-echo "- Cypress ${CYPRESS_VER}"
-echo "- NodeJS ${NODEJS_VER}"
-
-
-## Add Browser versions
+## Gather Browser versions
 BASE_IMAGE=$(grep FROM Dockerfile | cut -d ' ' -f 2)
 docker pull "${BASE_IMAGE}" > /dev/null 2>&1 || exit 1
 FF_VER=$(docker inspect ${BASE_IMAGE} | jq -r '.[0].ContainerConfig.Env | .[] | select(. | startswith("FF_VER="))' | cut -d '=' -f 2)
 CHROME_VER=$(docker inspect ${BASE_IMAGE} | jq -r '.[0].ContainerConfig.Env | .[] | select(. | startswith("CHROME_VER="))' | cut -d '=' -f 2)
 
-echo ""
-echo "## Browsers"
-echo "- Firefox ${FF_VER}"
-echo "- Chrome ${CHROME_VER}";
+## Generate everything
+cat <<EOF
+
+## Changelog
+${CHANGELOG}
+
+## Frameworks
+- Cypress ${CYPRESS_VER}
+- NodeJS ${NODEJS_VER}
+
+## Browsers
+- Firefox ${FF_VER}
+- Chrome ${CHROME_VER}
+
+### Build Info
+<details>
+
+- jobId: ${GITHUB_RUN_ID}
+- branch: ${BRANCH_NAME}
+
+</details>
+EOF
