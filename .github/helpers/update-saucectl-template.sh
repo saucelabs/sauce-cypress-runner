5bc6a98a5752902e8109c03870462b02789a90fe
Alex Plischke <alex.plischke@saucelabs.com>
2021-07-14 14:41:09 -0700

    Delete obsolete template publishing

--- .github/helpers/update-saucectl-template.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## Add Framework version
-CYPRESS_VER=$(< package-lock.json jq -r '.dependencies["cypress"].version')
-
-sed -E -i "s/^  version: (.*)$/  version: ${CYPRESS_VER}/g" .saucetpl/.sauce/config.yml
-git add .saucetpl/.sauce/config.yml
