a548788fa1f773fdb867bd9b57ca446fcc176dfe
Félix P <felix@saucelabs.com>
2022-01-27 11:52:47 +0100

    Add note for suite.config.headless

--- src/cypress-runner.js
+++ src/cypress-runner.js
@@ -102,6 +102,7 @@ const getCypressOpts = function (runCfg, suiteName) {
   const cypressCfg = JSON.parse(fs.readFileSync(cypressCfgFile, 'utf8'));
 
   let headed = true;
+  // suite.config.headless is kepts to keep backward compatibility.
   if (suite.headless || suite.config.headless) {
     headed = false;
   }
