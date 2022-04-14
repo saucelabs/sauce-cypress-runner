aed49fc677d170435a653118c3c0cd23a3cf5ba8
Tian Feng <tian.feng@saucelabs.com>
2022-04-14 09:35:10 -0700

    update according to comments

--- src/cypress-runner.js
+++ src/cypress-runner.js
@@ -139,7 +139,7 @@ const getCypressOpts = function (runCfg, suiteName) {
     headless: !headed,
     config: {
       testFiles: suite.config.testFiles,
-      ignoreTestFiles: suite.ignoreTestFiles || [],
+      ignoreTestFiles: suite.config.ignoreTestFiles || [],
       videosFolder: runCfg.resultsDir,
       screenshotsFolder: runCfg.resultsDir,
       video: shouldRecordVideo(),
