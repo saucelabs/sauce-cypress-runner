4419892af7e554086c89eaaba1b612755a401b0a
Mike Han <mike.han@saucelabs.com>
2022-10-17 11:13:03 -0600

    Make sure playwright-webkit deps are installed

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -12,6 +12,7 @@ cp "$(which node)" bundle/
 pushd bundle/
 npm cache clean --force
 PLAYWRIGHT_BROWSERS_PATH=0 npm ci --production
+PLAYWRIGHT_BROWSERS_PATH=0 npx playwright install-deps webkit
 ./node ./node_modules/cypress/bin/cypress verify
 # TODO: Add "saucectl" tests here
 popd
