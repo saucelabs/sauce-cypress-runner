7e22d075fd3089eccc44f3ca9229f2873e32e1a1
Félix P <felix@saucelabs.com>
2023-03-08 11:16:07 +0100

    Bump sauce-testrunner-utils@1.0.0 (#223)
    * Bump sauce-testrunner-utils@1.0.0

--- tests/post-release/cypress.config.js
+++ tests/post-release/cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
-    supportFile: false,
+    supportFile: 'cypress/support/e2e.js',
   },
 });
-
