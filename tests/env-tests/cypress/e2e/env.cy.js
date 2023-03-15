2e41c8e0401a1f7f7e466d98942000707087676b
Alex Plischke <alex.plischke@saucelabs.com>
2023-03-15 09:14:09 -0700

    Fix expected title

--- tests/env-tests/cypress/e2e/env.cy.js
+++ tests/env-tests/cypress/e2e/env.cy.js
@@ -8,6 +8,6 @@ context('Actions', function () {
     expect(Cypress.env('SAUCE_SUITE_NAME')).to.equal('default');
     expect(Cypress.env('SAUCE_ARTIFACTS_DIRECTORY')).to.equal('/home/runner/work/sauce-cypress-runner/sauce-cypress-runner/tests/env-tests/__assets__');
     cy.visit(Cypress.env('host'));
-    cy.title().should('eq', 'Join our Community | Sauce Labs');
+    cy.title().should('eq', 'Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing');
   });
 });
