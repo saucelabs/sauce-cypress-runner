9ae6fd6af4c23ccbf021451908ef015e8fc29e6b
Alex Plischke <alex.plischke@saucelabs.com>
2023-03-15 10:28:01 -0700

    Fix pipeline (#225)
    * Fix broken host
    
    * Fix expected title
    
    * Remove CI specific values from e2e tests
    
    * Bump saucectl action
    
    * Pass github token to saucectl action

--- tests/env-tests/cypress/e2e/env.cy.js
+++ tests/env-tests/cypress/e2e/env.cy.js
@@ -4,10 +4,9 @@ context('Actions', function () {
   it('should use host', function () {
     expect(Cypress.env('FOO')).to.equal('bar');
     expect(Cypress.env('VALUE')).to.equal('Some test value');
-    expect(Cypress.env('host')).to.equal('https://training.staging.saucelabs.net/');
+    expect(Cypress.env('host')).to.equal('https://saucelabs.com/');
     expect(Cypress.env('SAUCE_SUITE_NAME')).to.equal('default');
-    expect(Cypress.env('SAUCE_ARTIFACTS_DIRECTORY')).to.equal('/home/runner/work/sauce-cypress-runner/sauce-cypress-runner/tests/env-tests/__assets__');
     cy.visit(Cypress.env('host'));
-    cy.title().should('eq', 'Join our Community | Sauce Labs');
+    cy.title().should('eq', 'Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing');
   });
 });
