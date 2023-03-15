9689b8a0ce136deb633234b40be6de73a3a9d0ad
Alex Plischke <alex.plischke@saucelabs.com>
2023-03-15 09:09:48 -0700

    Fix broken host

--- tests/env-tests/cypress/e2e/env.cy.js
+++ tests/env-tests/cypress/e2e/env.cy.js
@@ -4,7 +4,7 @@ context('Actions', function () {
   it('should use host', function () {
     expect(Cypress.env('FOO')).to.equal('bar');
     expect(Cypress.env('VALUE')).to.equal('Some test value');
-    expect(Cypress.env('host')).to.equal('https://training.staging.saucelabs.net/');
+    expect(Cypress.env('host')).to.equal('https://saucelabs.com/');
     expect(Cypress.env('SAUCE_SUITE_NAME')).to.equal('default');
     expect(Cypress.env('SAUCE_ARTIFACTS_DIRECTORY')).to.equal('/home/runner/work/sauce-cypress-runner/sauce-cypress-runner/tests/env-tests/__assets__');
     cy.visit(Cypress.env('host'));
