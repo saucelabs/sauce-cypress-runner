0f271c317960a0be10b26fa9de6fa4d729a3d9e6
Felix P <felix@saucelabs.com>
2023-03-20 18:04:12 +0100

    Update integration target

--- tests/integration/integration-tests.sh
+++ tests/integration/integration-tests.sh
@@ -2,7 +2,7 @@ export SAUCE_USERNAME=''
 export SAUCE_ACCESS_KEY=''
 export TEST_VALUE='Some test value'
 
-node . -r tests/config-tests/sauce-runner.json -s "default"
-node . -r tests/config-tests/sauce-runner.json -s "webkit"
+node . -r tests/integration/config-tests/sauce-runner.json -s "default"
+node . -r tests/integration/config-tests/sauce-runner.json -s "webkit"
 
-node . -r tests/env-tests/sauce-runner.json -s "default"
+node . -r tests/integration/env-tests/sauce-runner.json -s "default"
