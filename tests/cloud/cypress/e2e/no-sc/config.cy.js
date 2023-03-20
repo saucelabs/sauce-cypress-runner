da511c579659c5dd41f51218b167f5e7bb768cb7
Felix P <felix@saucelabs.com>
2023-03-20 15:19:03 +0100

    Remove dep

--- tests/cloud/cypress/e2e/no-sc/config.cy.js
+++ tests/cloud/cypress/e2e/no-sc/config.cy.js
@@ -2,14 +2,12 @@
 
 context('Actions', function () {
   beforeEach(function () {
-    cy.step('open a page');
     cy.visit('https://example.cypress.io/commands/actions');
   });
 
   // https://on.cypress.io/interacting-with-elements
 
   it('should use cypress.json', function () {
-    cy.step('running assertions');
     expect(Cypress.env('foo')).to.equal('bar');
   });
 });
