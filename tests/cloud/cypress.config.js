852d12c87f5a7eae169a9804db384ad20781875d
Félix P <felix@saucelabs.com>
2023-03-21 08:31:11 +0100

    Enhance testing strategy (#226)

---
+++ tests/cloud/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    supportFile: 'cypress/support/e2e.js',
+    experimentalWebKitSupport: true,
+  },
+});
