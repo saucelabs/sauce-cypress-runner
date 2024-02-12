1159ed4e83db0333c069cd30a2bc12099d4a5114
Tian Feng <tian.feng@saucelabs.com>
2024-02-12 09:23:50 -0800

    Revert "try to fix e2e test :-/"
    This reverts commit f23b9d357ae88d0342f1eee71072a4fd4424c66a.

--- tests/integration/env-tests/cypress/e2e/env.cy.js
+++ tests/integration/env-tests/cypress/e2e/env.cy.js
@@ -4,7 +4,7 @@ context('Actions', function () {
   it('should use host', function () {
     expect(Cypress.env('FOO')).to.equal('bar');
     expect(Cypress.env('VALUE')).to.equal('Some test value');
-    expect(Cypress.env('host')).to.equal('https://saucelabs.com');
+    expect(Cypress.env('host')).to.equal('https://saucelabs.com/');
     expect(Cypress.env('SAUCE_SUITE_NAME')).to.equal('default');
     cy.visit(Cypress.env('host'));
     cy.title().should(
