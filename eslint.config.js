75eaf6baa56b6426b696bdfa9e5d7097d756d765
Alex Plischke <alex.plischke@saucelabs.com>
2024-06-12 10:58:09 -0700

    build: downgrade to Node.js 20 (#297)
    * build: downgrade to Node.js 20
    
    * deps: add @eslint/js
    
    * build: fix incorrect eslint configuration
    
    * deps: remove jest
    
    * build: optimize eslint config
    
    * ci: remove pre-push
    
    * ci: no unit-tests
    
    * ci: exclude macOS12 plus webkit

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,12 @@
 const ts = require('typescript-eslint');
 const js = require('@eslint/js');
 const prettier = require('eslint-config-prettier');
-const cypress = require('eslint-plugin-cypress');
-const jest = require('eslint-plugin-jest');
+const cypress = require('eslint-plugin-cypress/flat');
 
 module.exports = ts.config(
   js.configs.recommended,
   ...ts.configs.recommended,
   prettier,
-  {
-    plugins: {
-      cypress: cypress,
-      jest: jest,
-    },
-  },
   {
     ignores: ['lib/**'],
   },
@@ -26,6 +19,10 @@ module.exports = ts.config(
     },
   },
   {
+    files: ['tests/**/*.*js', 'tests/**/*.*ts'],
+    ...cypress.configs.recommended,
+  },
+  {
     languageOptions: {
       globals: {
         __dirname: true,
@@ -33,6 +30,9 @@ module.exports = ts.config(
         exports: true,
         module: true,
         require: true,
+        process: true,
+        NodeJS: true,
+        CypressCommandLine: true,
       },
     },
   },
