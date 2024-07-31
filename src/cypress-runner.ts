0ee71d2b96010b459936abac154ee90487933918
Alex Plischke <alex.plischke@saucelabs.com>
2024-07-31 12:33:26 -0700

    account for platform differences

--- src/cypress-runner.ts
+++ src/cypress-runner.ts
@@ -277,8 +277,8 @@ async function cypressRunner(
     nodeBin.startsWith('/Volumes/Sauce/node/') ||
     nodeBin.startsWith('D:\\node\\')
   ) {
-    nodeCtx.nodePath = 'node';
-    nodeCtx.npmPath = 'npm';
+    nodeCtx.nodePath = process.platform === 'win32' ? 'node.exe' : 'node';
+    nodeCtx.npmPath = process.platform === 'win32' ? 'npm.exe' : 'npm';
   }
 
   await prepareNpmEnv(runCfg, nodeCtx);
