310a4671a65abc622cd4290fef816a552840284e
Nitin <nitin.kumar@saucelabs.com>
2026-01-15 15:39:48 +0530

    change domain for testing

--- tests/integration/env-tests/cypress/e2e/env.cy.js
+++ tests/integration/env-tests/cypress/e2e/env.cy.js
@@ -1,15 +1,17 @@
 /// <reference types="cypress" />
 
-context('Actions', function () {
-  it('should use host', function () {
+context('Getting Started', function () {
+  const testName = 'cypress test';
+  it(testName, function () {
     expect(Cypress.env('FOO')).to.equal('bar');
     expect(Cypress.env('VALUE')).to.equal('Some test value');
-    expect(Cypress.env('host')).to.equal('https://saucelabs.com/');
+    expect(Cypress.env('host')).to.equal(
+      'http://devexpress.github.io/testcafe/example',
+    );
     expect(Cypress.env('SAUCE_SUITE_NAME')).to.equal('default');
     cy.visit(Cypress.env('host'));
-    cy.title().should(
-      'eq',
-      'Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing',
-    );
+    cy.get('#developer-name').type('devx');
+    cy.get('#submit-button').click();
+    cy.get('#article-header').should('have.text', 'Thank you, devx!');
   });
 });
