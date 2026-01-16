d3ea269b40b0ad3f1a57ba01941244dc9fde0316
Nitin <nitin.kumar@saucelabs.com>
2026-01-16 18:30:18 +0530

    add version specific playwright in macos bundle job

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -13,14 +13,6 @@ pushd bundle/
 npm cache clean --force
 PLAYWRIGHT_BROWSERS_PATH=0 npm ci --production
 PLAYWRIGHT_BROWSERS_PATH=0 npx playwright install-deps webkit
-# npm run build
-# ./node ./node_modules/cypress/bin/cypress verify
-
-export PLAYWRIGHT_BROWSERS_PATH=0
-export PLAYWRIGHT_SKIP_BROWSER_GC=1
-npm ci --production
-# Assuming bundle.sh creates a 'bundle' folder with package.json
-PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=mac13 npx playwright install webkit
 npm run build
 ./node ./node_modules/cypress/bin/cypress verify
 # TODO: Add "saucectl" tests here
