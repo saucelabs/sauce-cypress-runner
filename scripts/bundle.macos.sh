d705233dbcd6949893f9bfa055bb6673448938b8
arvind.singh <arvind.singh@saucelabs.com>
2026-03-25 16:04:49 +0530

    fix: pin playwright-webkit to 1.57.0 and add to ncu reject list

--- scripts/bundle.macos.sh
+++ scripts/bundle.macos.sh
@@ -8,7 +8,7 @@ bash ./scripts/bundle.sh
 export PLAYWRIGHT_BROWSERS_PATH=0
 export PLAYWRIGHT_SKIP_BROWSER_GC=1
 cd bundle
-PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=mac14 npx playwright install webkit
+PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=mac13 npx playwright install webkit
 cd ..
 
 # Archive Bundle with symlinks
